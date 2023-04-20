ebcf295c512e8a7183fc4fcb64f0a3a0aa22ef74
Melissa Appadoo <114985119+melissaappadoo@users.noreply.github.com>
2023-04-20 14:54:08 +0100

    week 8

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
